5088d860f6e3467fe8393e974522bf434451812e
Manuel Pras <manuel.pras@deutschebahn.com>
2020-01-30 19:45:08 +0100

    add DB and api

--- Setup.md
+++ Setup.md
@@ -16,3 +16,34 @@
     * Create or update Cognito user pool groups
     * add the following User Pool Group: Admins
     * done
+* `amplify push`
+* `amplify publish`
+    * Now you can sign up and sign in
+* Manage Admins
+    * go to https://eu-central-1.console.aws.amazon.com/cognito/users/?region=eu-central-1#/pool/eu-central-1_KpQRRuw1m/groups/Admins?_k=dfzg0l
+    * click on `Add users`
+    * click on `+` in front of user to add
+* `amplify add api`
+    * select REST
+    * path: /items
+    * friendly name: fuchsstreckenstatusapi
+    * AWS Lambda function name: fuchsstreckenstatus
+    * select `CRUD function for Amazon DynamoDB table`
+    * select `Create a new DynamoDB table`
+    * fiendly name: fuchsstreckenstatusdynamo
+    * table name: fuchsstreckenstatusdynamo
+    * add column: status
+    * data type: string
+    * answer the rest with `no`
+    * Do you want to edit the local lambda function now?: `Yes`
+    * just hit enter in the console
+    * Restrict API access: `Yes`
+    * Restrict access by?: `Both`
+    * Who should have access?: `Authenticated and Guest users`
+    * What kind of access do you want for Authenticated users?: select only read
+    * What kind of access do you want for Guest users?: select only read
+    * Select groups: Admins
+    * What kind of access do you want for Admins users?: select all 4 options
+    * done
+* `amplify push`
+* `amplify publish`
--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Amplify, { API } from "aws-amplify";
+import awsconfig from "./aws-exports";
+import { withAuthenticator } from "aws-amplify-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+Amplify.configure(awsconfig);
+
+const apiUrl = "fuchsstreckenstatusapi";
+const Status = {
+  green: "Green",
+  yellow: "Yellow",
+  red: "Red"
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { items: [] };
+  }
+
+  post = async () => {
+    console.log("calling api");
+    const response = await API.post(apiUrl, "/items", {
+      body: {
+        status: Status.green
+      }
+    });
+    alert(JSON.stringify(response, null, 2));
+  };
+
+  get = async () => {
+    console.log("calling api");
+    const response = await API.get(apiUrl, "/items/object/1");
+    alert(JSON.stringify(response, null, 2));
+  };
+
+  list = async () => {
+    console.log("calling api");
+    const response = await API.get(apiUrl, "/items");
+    this.setState({ items: response });
+    console.log(this.state.items);
+  };
+
+  render() {
+    const { items } = this.state;
+    return (
+      <div className="App">
+        <button onClick={this.post}>POST</button>
+        <button onClick={this.get}>GET</button>
+        <button onClick={this.list}>LIST</button>
+        <h1>List:</h1>
+        <ul>
+          {items.map(item => (
+            <li>item</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withAuthenticator(App, true);
