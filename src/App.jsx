79f7be6a3efcf247eb0d2a10262d535fb8f0c208
Manuel Pras <manuel.pras@deutschebahn.com>
2020-02-01 18:25:00 +0100

    customize appbar + refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { Grommet, Main, Heading, Paragraph, Button, Text } from "grommet";
+import { Logout } from "grommet-icons";
 import Amplify from "aws-amplify";
 import awsconfig from "./aws-exports";
 import { withAuthenticator } from "aws-amplify-react";
-import { Grommet, Main, Heading, Paragraph } from "grommet";
 import { StatusForm } from "./StatusForm";
 import { ErrorBoundary } from "./ErrorBoundary";
 import { fetchCurrentStatus, saveStatus } from "./api";
 import { StatusDisplay } from "./StatusDisplay";
 import { PopupBanner } from "./PopupBanner";
+import { Auth } from "aws-amplify";
+import { AppBar } from "./AppBar";
+
+Amplify.configure(awsconfig);
 
 const theme = {
   global: {
@@ -27,16 +32,32 @@ const newContentText = `Es gibt neuen Inhalt.
 Um ihn zu sehen, schließe diesen Tab (wenn du die App im Browser nutzt)
 oder schieße die App ab (wenn du sie dem Homescreen hinzgefügt hast).`;
 
-Amplify.configure(awsconfig);
-
-class App extends React.Component {
+export class App extends React.Component {
+  get isLoggedIn() {
+    return this.props.authState === "signedIn";
+  }
+  get isAdmin() {
+    if (!this.props.authData) {
+      return false;
+    }
+    const cognitoGroups = this.props.authData.signInUserSession.idToken.payload[
+      "cognito:groups"
+    ];
+    const isAdmin =
+      !!cognitoGroups && cognitoGroups.some(group => group === "Admins");
+    return isAdmin;
+  }
+  get userName() {
+    if (!this.props.authData) {
+      return "?";
+    }
+    return this.props.authData.username;
+  }
   constructor(props) {
     super(props);
     props.swConfig.onUpdate = this.onSwUpdate;
     props.swConfig.onSuccess = this.onSwSuccess;
     this.state = {
-      hasErrors: false,
-      isAdmin: false,
       showNewContentBanner: false,
       showOfflineReadyBanner: false
     };
@@ -50,57 +71,64 @@ class App extends React.Component {
     this.setState({ showOfflineReadyBanner: true });
   };
 
+  signOut = async () => {
+    await Auth.signOut();
+  };
+
   async componentDidMount() {
-    const cognitoGroups = this.props.authData.signInUserSession.idToken.payload[
-      "cognito:groups"
-    ];
-    const isAdmin =
-      !!cognitoGroups && cognitoGroups.some(group => group === "Admins");
-    this.setState({ isAdmin });
     await this.refreshStatus();
   }
 
   handleSubmit = async status => {
-    this.setState({ hasErrors: false });
+    this.setState({ error: undefined });
     try {
       await saveStatus(status);
       await this.refreshStatus();
     } catch (error) {
-      this.setState({ hasErrors: true });
+      this.setState({ error });
     }
   };
 
   async refreshStatus() {
-    this.setState({ hasErrors: false });
+    this.setState({ error: undefined });
     try {
       const status = await fetchCurrentStatus();
       this.setState({ status });
     } catch (error) {
-      this.setState({ hasErrors: true });
+      this.setState({ error });
     }
   }
 
   render() {
     const {
       status,
-      hasErrors,
-      isAdmin,
+      error,
       showOfflineReadyBanner,
       showNewContentBanner
     } = this.state;
     return (
       <ErrorBoundary>
         <Grommet theme={theme} full>
+          <AppBar>
+            <Text>Hi {this.userName}!</Text>
+            <Button
+              label="Ausloggen"
+              icon={<Logout />}
+              onClick={this.signOut}
+              justify="start"
+              alignSelf="end"
+            />
+          </AppBar>
           <Main flex pad="large" fill overflow={{ horizontal: "hidden" }}>
             <Heading>Streckenstatus</Heading>
             <StatusForm
-              isAdmin={isAdmin}
+              isAdmin={this.isAdmin}
               status={status}
               onSubmit={this.handleSubmit}
             />
-            {hasErrors && (
+            {!!error && (
               <Paragraph margin="none" color="status-critical">
-                Es ist ein Fehler aufgetreten...
+                Es ist ein Fehler aufgetreten: "{error}"
               </Paragraph>
             )}
             <StatusDisplay status={status} />
@@ -121,4 +149,4 @@ class App extends React.Component {
   }
 }
 
-export default withAuthenticator(App, true);
+export default withAuthenticator(App, false);
